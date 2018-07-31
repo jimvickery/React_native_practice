70f0f34d29851ed9fe83d698004c05418849db2a
Jim Vickery <jimrv8@yahoo.com>
2018-07-30 22:23:11 -0500

    saved layout Blue and light gray

--- App.js
+++ App.js
@@ -61,42 +61,99 @@
 // skip this line if using Create React Native App
 // AppRegistry.registerComponent('AwesomeProject', () => Bananas);
 
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, View } from 'react-native';
+
+// class Blink extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isShowingText: true};
+
+//     // Toggle the state every second
+//     setInterval(() => {
+//       this.setState(previousState => {
+//         return { isShowingText: !previousState.isShowingText };
+//       });
+//     }, 1000);
+//   }
+
+//   render() {
+//     let display = this.state.isShowingText ? this.props.text : ' ';
+//     return (
+//       <Text>{display}</Text>
+//     );
+//   }
+// }
+
+// export default class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Blink text='I love to blink' />
+//         <Blink text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' />
+//       </View>
+//     );
+//   }
+// }
+
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
+
+
+
+// import React, { Component } from 'react';
+// import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+// export default class LotsOfStyles extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text style={styles.red}>just red</Text>
+//         <Text style={styles.bigblue}>just bigblue</Text>
+//         <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+//         <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   bigblue: {
+//     color: 'blue',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//   },
+//   red: {
+//     color: 'red',
+//   },
+// });
+
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
+
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <Blink text='I love to blink' />
-        <Blink text='Yes blinking is so great' />
-        <Blink text='Why did they ever take this out of HTML' />
-        <Blink text='Look at me look at me look at me' />
-      </View>
+      <View style={{flex: 1}}>
+        <View style={{flex: 2, backgroundColor: 'darkblue'}} />
+        <View style={{flex: 3, backgroundColor: 'lightgray'}} />
+        <View style={{flex: 1, backgroundColor: 'darkblue'}} />
+        <View style={{flex: 3, backgroundColor: 'lightgray'}} />
+        <View style={{flex: 1, backgroundColor: 'darkblue'}} />
+        <View style={{flex: 3, backgroundColor: 'lightgray'}} />
+        <View style={{flex: 1, backgroundColor: 'darkblue'}} />
+        <View style={{flex: 3, backgroundColor: 'lightgray'}} />
+        <View style={{flex: 1, backgroundColor: 'darkblue'}} />
+
+   </View>
     );
   }
 }
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
