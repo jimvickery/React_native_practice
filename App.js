a3e1634c19d671f3b59f1bc34f121d3616929117
Jim Vickery <jimrv8@yahoo.com>
2018-07-30 23:14:03 -0500

    align items

--- App.js
+++ App.js
@@ -171,7 +171,8 @@ export default class FlexDimensionsBasics extends Component {
       <View style={{
         flex: 1, 
         flexDirection: 'column',
-        justifyContent: "space-between",
+        justifyContent: "center",
+        alignItems: 'center',
         }}>
         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
