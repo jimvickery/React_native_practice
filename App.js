bc0defb5361a6977b6204de1d43984cd5746cb97
Jim Vickery <jimrv8@yahoo.com>
2018-07-31 22:41:50 -0500

    added image prop

--- App.js
+++ App.js
@@ -435,47 +435,65 @@
 // AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
 
 
-import React, { Component } from 'react';
-import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+// import React, { Component } from 'react';
+// import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
 
-export default class SectionListBasics extends Component {
+// export default class SectionListBasics extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <SectionList
+//           sections={[
+//             {title: 'D', data: ['Devin']},
+//             {title: 'J', data: ['Jackson', 'Jill', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+//           ]}
+//           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+//           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+//           keyExtractor={(item, index) => index}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//    flex: 1,
+//    paddingTop: 22
+//   },
+//   sectionHeader: {
+//     paddingTop: 2,
+//     paddingLeft: 10,
+//     paddingRight: 10,
+//     paddingBottom: 2,
+//     fontSize: 14,
+//     fontWeight: 'bold',
+//     backgroundColor: 'rgba(247,247,247,1.0)',
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 18,
+//     height: 44,
+//   },
+// })
+
+// // skip this line if using Create React Native App
+// AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);
+
+
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
+
+export default class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <SectionList
-          sections={[
-            {title: 'D', data: ['Devin']},
-            {title: 'J', data: ['Jackson', 'Jill', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        />
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-})
-
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
